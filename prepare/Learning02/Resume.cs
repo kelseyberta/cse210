ea97f6bca11d34cde391e70043e017e4b1076e94
kelseyberta <ber20002@byui.edu>
2023-01-09 15:08:24 -0500

    Learning Activity W2

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Resume{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void Display(){
+        Console.WriteLine($"Name:{_name}");
+        Console.WriteLine($"Jobs:{_jobs}");
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
+    }
+}
