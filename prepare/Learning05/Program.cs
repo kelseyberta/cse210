b0962989fbe2010ae09e61f0283518e7530e333b
kelseyberta <ber20002@byui.edu>
2023-01-31 16:41:19 -0500

    W5 Learning Activty

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square ("blue", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle ("red", 3, 5);
+        shapes.Add(s2);
+
+        Circle s3 =  new Circle ("green", 2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }    
     }
 }
